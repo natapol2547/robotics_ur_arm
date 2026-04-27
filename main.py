48fdfba86ba0a76a11c7e7b0695b890094e467bc
MonochromePie <popularmmos2548@gmail.com>
2026-04-27 15:02:04 +0700

    Clean up some code, remove inappropiate comment for submission. Remove LeadLag compensator from main code as it isnt used in the real one. Remove old testing file (pypylon)

--- main.py
+++ main.py
@@ -1,52 +1,51 @@
 from ur_arm import Robot, pid
 from ur_arm import Camera
-from ur_arm import llc
 import numpy as np
 import cv2
 
 
-ROBOT_IP = "10.10.0.104"
+ROBOT_IP = "10.10.0.14"
 LOWER_LAB = np.array([137, 136, 112])
 UPPER_LAB = np.array([234, 176, 149])
 
-
+BOX_GRAB_HEIGHT = 0.166
+ADJUSTMENT_HEIGHT_THRESHOLD = BOX_GRAB_HEIGHT + 0.05
+ADJUSTMENT_CENTER_THRESHOLD = 80
+PID_GAIN = 1
 
 def main():
-    with Robot(robot_ip=ROBOT_IP, grip_force=50) as robot:
+    with Robot(robot_ip=ROBOT_IP, grip_force=50, move_speed=0.8) as robot:
 
         print("Activating gripper...")
         robot.activate_gripper()
         
         robot.move([0.116, -0.3, 0.2, 0, -3.143, 0], blocking=True)  #initial position according to project spec
-        cv2.waitKey(2000)  
-        robot.move([0.3, -0.32, -0.085, 2.25, 2.25, 0], blocking=True)  #move above conveyor belt
+        cv2.waitKey(500)  
+        input("Press Enter to start the box grabbing process...")  # Wait for user input before starting
+        robot.move([0.162, -0.318, 0.243, 2.276, 2.172, 0], blocking=True)  #move above conveyor belt
 
-        TIME_STEP = 0.001
-        pid_x = pid.PID(0.001, 0.001/420, 0)
-        pid_y = pid.PID(0.001, 0.0, 0.0)
-        llc_x = llc.leadlagcompensator(K=0.00001, T1=0.001, T2=0.002, Ts=TIME_STEP)
-        llc_y = llc.leadlagcompensator(K=0.00001, T1=0.001, T2=0.002, Ts=TIME_STEP)
+        SAMPLING_RATE = 0.01
+        pid_x = pid.PID(0.0003, 0.0, 0)
+        pid_y = pid.PID(0.0003, 0.0, 0.0)
 
         cam = Camera(debug=True)
         cam_width, cam_height = cam.get_cam_res()
         print("Camera resolution: ", (cam_width, cam_height))
-        cv2.waitKey(2000)
-
-        box_grab_count = 0
+        cv2.waitKey(500)
 
 
-        ########################## PID + LEAD LAG COMPENSATOR CONTROL LOOP ##########################        
+        ########################## PID CONTROL LOOP ##########################        
         while True:
             while True:
                 _, _, z, _, _, _ = robot.get_tcp_pose()
-                if z < 0.27:
+                print(f"Current Z position: {z:.4f} m")
+                if z < BOX_GRAB_HEIGHT:
                     break
     
                 block = cam.get_block_pos(LOWER_LAB, UPPER_LAB, 5000)
 
                 if block is not None:
                     pos_x, pos_y, width, height, angle = block
-                    # print(f"Block position found: x={pos_x}, y={pos_y}, width={width}, height={height}, angle={angle}")
 
                     cam_center_x = cam_width / 2
                     cam_center_y = cam_height / 2
@@ -55,57 +54,49 @@ def main():
                     target_x = cam_center_x  
                     target_y = cam_center_y + offset_mag 
 
-                    #yes the pos_x and pos_y are swapped, this shit is intentional
+                    #The pos_x and pos_y are swapped due to camera orientation
                     x_pid_output = pid_x.compute(target_y, pos_y)
                     y_pid_output = pid_y.compute(target_x, pos_x)
 
-                    x_llc_output = llc_x.compute(target_y - pos_y)
-                    y_llc_output = llc_y.compute(target_x - pos_x)
-
-                    x_llc_output = min(0.05, max(-0.05, x_llc_output))
-                    y_llc_output = min(0.05, max(-0.05, y_llc_output))
-
-                    x_output = x_pid_output + x_llc_output
-                    y_output = y_pid_output + y_llc_output
-                    print(f"PID + Lead Compensator Output: x={x_output}, y={y_output}")                    
-
+                    x_output = x_pid_output * PID_GAIN
+                    y_output = y_pid_output * PID_GAIN
+                          
                     #high pass filter, threshold to prevent jitter
                     if abs(x_output) < 0.001:
                         x_output = 0
                     if abs(y_output) < 0.001: 
                         y_output = 0
 
-                    # Limit speed
-                    speed = [x_output, y_output, -0.05]
+                    z_speed = 0
+                    if z > ADJUSTMENT_HEIGHT_THRESHOLD:
+                        z_speed = -0.08  # Move down if above threshold
+                    elif abs(pos_x - target_x) < ADJUSTMENT_CENTER_THRESHOLD and abs(pos_y - target_y) < ADJUSTMENT_CENTER_THRESHOLD:
+                        z_speed = -0.08  # Move down if centered
+
+
+                    # Limit and Set Speed
                     speed = [max(-0.15, min(0.15, v)) for v in speed]
-                    # Test speed
+                    speed = [x_output, y_output, z_speed]
                     robot.set_speed(speed)
 
-                    cv2.waitKey(int(TIME_STEP*1000))  # Wait
+                    cv2.waitKey(int(SAMPLING_RATE*1000)) 
 
                 else:
-                    robot.set_speed([0, 0, 0])  # Stop movement if no block is found
-                    llc_x.reset()
-                    llc_y.reset()
+                    # Stop movement if no block is found
+                    robot.set_speed([0, 0, 0])  
                     cv2.waitKey(10)
             
+            # Stop the robot and close the gripper to grab the box
             robot.rtde_c.speedStop()
             robot.close_gripper()
-
-            box_grab_count += 1
-            print(f"Box grabbed! Total count: {box_grab_count}")
             
-            robot.move_offset([0, 0, 0.3], blocking=True)  # Move up after gripping
-            cv2.waitKey(3000)
+            # Move up after gripping and open the gripper to release the box
+            robot.move_offset([0, 0, 0.1], blocking=True)  
+            cv2.waitKey(1000)
             robot.open_gripper()
-            
-            cv2.waitKey(2500)
-            
 
-            BOX_GRAB_LIMIT = 2
-            if box_grab_count >= BOX_GRAB_LIMIT:
-                print(f"Grabbed {BOX_GRAB_LIMIT} boxes, ending program.")
-                break
+            #move above conveyor belt to look for next box
+            robot.move([0.162, -0.318, 0.243, 2.276, 2.172, 0], blocking=True)  
         
         
         
